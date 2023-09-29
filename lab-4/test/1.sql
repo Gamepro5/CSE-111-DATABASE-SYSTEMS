b8b6037ce666eaf94cfba84616359b21a3c54653
Gamepro5 <gamepro543@gmail.com>
2023-09-29 14:36:01 -0700

    progress

--- lab-4/test/1.sql
+++ lab-4/test/1.sql
@@ -1,1 +1,5 @@
 .headers on
+select n_name as country, count(o_orderkey) as cnt
+from orders, customer, nation, region
+where c_custkey = o_custkey and c_nationkey = n_nationkey and n_regionkey = r_regionkey and r_name = "EUROPE"
+group by n_name
--- lab-4/test/2.sql
+++ lab-4/test/2.sql
@@ -1,1 +1,5 @@
 .headers on
+select c_name as customer, count(o_orderkey) as cnt
+from customer, nation, orders
+where c_custkey = o_custkey and c_nationkey = n_nationkey and n_name == "EGYPT" and STRFTIME('%Y', orders.o_orderdate) == '1992'
+group by c_name
